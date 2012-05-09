bed1aa890bfd2e39db0085a1749beb460e69cc8a
Daniel Taschik <daniel@taschik.de>
2012-05-09 17:00:20 -0400

    added hello world

--- README.md
+++
@@ -1,2 +0,0 @@
-RAMCloud-load-manager
-=====================
---
+++ hello-world/src/helloworld.cc
@@ -0,0 +1,37 @@
+#include "RamCloud.h"
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    RAMCloud::RamCloud cloud("fast+udp:host=127.0.0.1,port=12246");
+    
+    const char* kTableName = "hello_world_table";
+
+    cloud.createTable(kTableName);
+
+    const uint64_t tableId = cloud.getTableId(kTableName);
+
+    //write to RAMCloud, key "0" (which has the keylength 1) has the value HalloHPI
+    cloud.write(tableId, "0", 1, "Hallo Christian");
+
+    RAMCloud::Buffer buffer;
+
+    cloud.read(tableId, "0", 1, &buffer);
+
+    uint16_t length = buffer.getTotalLength();
+
+    char str[length];
+
+    uint32_t returned = buffer.copy(0, buffer.getTotalLength(), str);
+
+
+    std::cout << "Greetings from RAMCloud: " << str << std::endl;
+    
+    return 0;
+}
